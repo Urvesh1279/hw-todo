f117d4ef4bca0d45204433e5274b3f1f35fd7e6f
Urvesh1279 <urveshmukeshbhai1279@gmail.com>
2024-03-19 16:17:27 +0530

    msg

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-console.log("this is app.js");
+const { appConfig } = require("./package.json");
+const express = require("express");
+const { getDBConnection } = require("./utils");
+const chalk = require("chalk");
+//middlewars
+const { requestLogger } = require("./middlewares/requestLogger");
+//routers
+const { routerAPI } = require("./routers/routerAPI");
+const { routerUI } = require("./routers/routerUI");
+const bodyParser = require("body-parser");
+
+//instance of express
+const app = express();
+
+// parse application/x-www-form-urlencoded
+//post method body parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+app.set("view engine", "pug");
+
+//in case you want to change view directory
+//app.set('views', libPath.join(__dirname, '/templates'));
+
+app.use(requestLogger);
+
+//Api Router
+app.use("/api", routerAPI);
+//Web page
+app.use(routerUI);
+//Web page
+
+getDBConnection()
+  .then((dbToDo) => {
+    console.log(chalk.green("[+]DB is Connected"));
+    app.listen(appConfig.port, () => {
+      console.log(chalk.green(`[+]App is Running on Port ${appConfig.port}`));
+    });
+  })
+  .catch((e) => {
+    console.log(chalk.red(`[-]DB Connection Failed ${e}`));
+  });
---
+++ routers/routerAPI.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const routerAPI = express.Router();
+
+const { routesUser } = require("./routesUser");
+
+//send back sepcific user
+routerAPI.get("/user/:id", routesUser.getSingleUser);
+routerAPI.get("/user", routesUser.getAllUsers);
+routerAPI.post("/user", routesUser.createUser);
+routerAPI.put("/user/:id", routesUser.updateUser);
+routerAPI.delete("/user/:id", routesUser.deleteUser);
+
+routerAPI.get("/todo", (req, res, next) => {
+  res.send({ todo: [] });
+});
+routerAPI.all("*", (req, res, next) => {
+  res.send({ error: "invalid api" });
+});
+
+module.exports.routerAPI = routerAPI;
